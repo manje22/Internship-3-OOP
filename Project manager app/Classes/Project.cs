0184db9995634a1a2e2bde39c115bdcf155c03d0
manje22 <uvodicmariela@gmail.com>
2024-11-19 18:55:16 +0100

    created classes Project and Task

--- Project manager app/Classes/Project.cs
+++ Project manager app/Classes/Project.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_manager_app.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace Project_manager_app.Classes
 {
-    internal class Project
+    public class Project
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Status Status { get; set; }
+
+        public Project(string name, string description, DateTime startDate) 
+        {
+            Name = name;
+            Description = description;
+            StartDate = startDate;
+            EndDate = DateTime.MaxValue;
+            Status = Status.Waiting;
+        }
     }
 }
--- Project manager app/Classes/Task.cs
+++ Project manager app/Classes/Task.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_manager_app.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace Project_manager_app.Classes
 {
-    internal class Task
+    public class Task
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public Status Status { get; set; }
+        public int ExpectedDuration { get; set; }
+        public Project ParentProject { get; set; }
+
+        public Task(string name, string description, Project parentProject)
+        {
+            Name = name;
+            Description = description;
+            DueDate = DateTime.MaxValue;
+            Status = Status.Active;
+            ExpectedDuration = int.MaxValue;
+            ParentProject = parentProject;
+        }
     }
 }
