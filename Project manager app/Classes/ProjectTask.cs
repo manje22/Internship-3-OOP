8b8f0e4fb8d48f49b292d1fc9e7686f184b45edc
manje22 <uvodicmariela@gmail.com>
2024-11-20 11:50:11 +0100

    add new class projectTask because the old one was not visible anymore, not sure what happened exactly

--- Project manager app/Classes/ProjectTask.cs
+++ Project manager app/Classes/ProjectTask.cs
@@ -7,23 +7,21 @@ using System.Threading.Tasks;
 
 namespace Project_manager_app.Classes
 {
-    public class Task
+    public class ProjectTask
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime DueDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public Status Status { get; set; }
-        public int ExpectedDuration { get; set; }
-        public Project ParentProject { get; set; }
 
-        public Task(string name, string description, Project parentProject)
+        public ProjectTask(string name, string description, DateTime startDate)
         {
             Name = name;
             Description = description;
-            DueDate = DateTime.MaxValue;
-            Status = Status.Active;
-            ExpectedDuration = int.MaxValue;
-            ParentProject = parentProject;
+            StartDate = startDate;
+            EndDate = DateTime.MaxValue;
+            Status = Status.Waiting;
         }
     }
 }
