979059f55c0b7345a567b589e8d231f50870c847
manje22 <uvodicmariela@gmail.com>
2024-11-20 12:07:24 +0100

    created main dictionary

--- Project manager app/Program.cs
+++ Project manager app/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Project_manager_app.Classes;
 
 namespace Project_manager_app
 {
@@ -12,7 +12,7 @@ namespace Project_manager_app
     {
         static void Main(string[] args)
         {
-            
+            Dictionary<Project, List<ProjectTask>> mainDictionary = new Dictionary<Project, List<ProjectTask>>();
         }
     }
 }
