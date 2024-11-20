48393e69158e46e0e097131194135d801fcbe087
manje22 <uvodicmariela@gmail.com>
2024-11-20 12:27:28 +0100

    Made outline for 3 main method (MainMenu, ProjectMenu and Project task menu)

--- Project manager app/Program.cs
+++ Project manager app/Program.cs
@@ -10,6 +10,33 @@ namespace Project_manager_app
 {
     internal class Program
     {
+        static void MainMenu()
+        {
+            Console.WriteLine("1. Ispis svih projekata s pripadajućim zadacima");
+            Console.WriteLine("2. Dodavanje novog projekta");
+            Console.WriteLine("3. Brisanje projekta");
+            Console.WriteLine("4. Prikaz svih zadataka s rokom u sljedećih 7 dana");
+            Console.WriteLine("5. Prikaz  projekata filtriranih po status (samo aktivni, ili samo završeni, ili samo na čekanju)");
+            Console.WriteLine("6. Upravljanje pojedinim projektom");
+            Console.WriteLine("7. Upravljanje pojedinim zadatkom0");
+        }
+
+        static void ProjectMenu()
+        {
+            Console.WriteLine("1. Ispis svih zadataka unutar odabranog projekta");
+            Console.WriteLine("2. Prikaz detalja odabranog projekta");
+            Console.WriteLine("3. Uređivanje statusa projekta");
+            Console.WriteLine("4. Dodavanje zadatka unutar projekta");
+            Console.WriteLine("5. Brisanje zadatka iz projekta");
+            Console.WriteLine("6. Prikaz ukupno očekivanog vremena potrebnog za sve aktivne zadatke u projektu");
+        }
+
+        static void ProjectTaskMenu()
+        {
+            Console.WriteLine("1. Prikaz detalja odabranog zadatka");
+            Console.WriteLine("2. Uređivanje statusa zadatka");
+        }
+
         static void Main(string[] args)
         {
             Dictionary<Project, List<ProjectTask>> mainDictionary = new Dictionary<Project, List<ProjectTask>>();
